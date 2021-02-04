3e3eb8055fa848a337a9de97deafff8cb42917d9
rdev72 <60402153+rdev72@users.noreply.github.com>
2021-02-04 15:13:29 +0530

    Update nodemailer.js
    mail id changed

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -6,21 +6,21 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "rohandev.dev74@gmail.com",
-    pass: "Devroh@n9080"
+    user: "test.test@gmail.com",
+    pass: "test123"
   }
 });
 // Accept mail
 const mailOptions1 = {
-  from: '"rohan" <rohandev.dev74@gmail.com>',
-  to: "rohandev.dev72@gmail.com",
+  from: '"test" <test.test@gmail.com>',
+  to: "test2.test@gmail.com",
   subject: "Mail for Application",
   text: "your application is accepted for licence"
 };
 //reject mail
 const mailOptions2 = {
-  from: '"rohan" <rohandev.dev74@gmail.com>',
-  to: "rohandev.dev72@gmail.com",
+  from: '"test" <test.test@gmail.com>',
+  to: "test2.test@gmail.com",
   subject: "Mail for Application",
   text: "your application is rejected for licence"
 };
